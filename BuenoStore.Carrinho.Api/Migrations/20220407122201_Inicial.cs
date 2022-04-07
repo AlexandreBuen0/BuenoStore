a9f42ca44d98de68fb2855163049fa8ce88d9845
Alexandre Bueno <alexandre.sbueno@ambevtech.com.br>
2022-04-07 10:25:31 -0300

    Migração inicial Carrinho

---
+++ BuenoStore.Carrinho.Api/Migrations/20220407122201_Inicial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuenoStore.Carrinho.Api.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrinho",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinho", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoItens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProdutoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CarrinhoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoItens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoItens_Carrinho_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinho",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoItens_CarrinhoId",
+                table: "CarrinhoItens",
+                column: "CarrinhoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoItens");
+
+            migrationBuilder.DropTable(
+                name: "Carrinho");
+        }
+    }
+}
