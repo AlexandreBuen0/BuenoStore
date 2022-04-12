78e8eb2fff994f69e5b6ca1c8736272d68de0a0f
Alexandre Bueno <alexandre.sbueno@ambevtech.com.br>
2022-04-11 22:28:43 -0300

    Configuração do Token e resiliência

---
+++ BuenoStore.BuildingBlocks/Token/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BuenoStore.BuildingBlocks.Token
+{
+    public class AppSettings
+    {
+        public string Chave { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string SistemaEmissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
